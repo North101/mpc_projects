39cd30aaf1af5fbc76e22a8a40da3cd9e6584e39
Alexander Wilde <alexander.wilde87@gmail.com>
2023-08-07 21:59:28 +0100

    Hide ProjectCard body and disable link to project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,21 +9,18 @@ interface ProjectCardProps {
 }
 
 export const ProjectCard = ({ project }: ProjectCardProps) => (
-  <Card style={{ height: 200, overflow: "hidden" }}>
+  <Card>
     <Card.Header as="h5">
       <div style={{ display: "flex" }}>
-        <Card.Link className="text-truncate" href={`/project/${project.name}`} style={{ flex: 1, alignSelf: "center" }}>
+        <Card.Link className="text-truncate" href={project.website ?? undefined} style={{ flex: 1, alignSelf: "center" }}>
           {project.name}
         </Card.Link>
-        <Button style={{ width: 32, height: 32 }} variant="outline-primary" size="sm">
+        <Button style={{ width: 32, height: 32, marginLeft: 4 }} variant="outline-primary" size="sm">
           <a href={`/projects/${project.filename}`} download={true} style={{ color: "inherit" }}><CloudArrowDown /></a>
         </Button>
       </div>
       <Card.Subtitle className="text-truncate"><ProjectAuthors authors={project.authors} /></Card.Subtitle>
     </Card.Header>
-    <Card.Header><ProjectTags tags={project.tags} /></Card.Header>
-    <Card.Body>
-      <Card.Text>{project.description}</Card.Text>
-    </Card.Body>
+    <Card.Footer><ProjectTags tags={project.tags} /></Card.Footer>
   </Card>
 );
