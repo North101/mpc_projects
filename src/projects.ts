e5c34f80545cdcca59c869778afae3ab204e31ec
Alexander Wilde <alexander.wilde87@gmail.com>
2023-08-09 06:24:59 +0100

    Only fetch projects once

--- src/projects.ts
+++ src/projects.ts
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import { Project } from "./types";
 
+let projects: Promise<Project[]>;
+
+const fetchProjects = async () => {
+  const r = await fetch('/projects.json');
+  return await r.json();
+}
+
 export const useProjects = () => {
   const [data, setData] = useState<Project[] | undefined>();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const r = await fetch('/projects.json');
-      setData(await r.json());
-    };
-
-    fetchData();
+    projects ??= fetchProjects();
+    projects.then(setData);
   }, []);
 
   return data;
