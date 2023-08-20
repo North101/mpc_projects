7f4a0e2b7e6a3402422ff894d9ddd623b7d9b241
BrightBold <kelley@brightbold.com>
2023-08-19 21:08:27 -0400

    add Intro.tsk, delete yarn.lock

---
+++ src/components/Intro.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+
+export const Intro = () => {
+  return (
+    <div className='intro w-75 m-auto'>
+      <p>&ldquo;It&rsquo;s just one little core set,&rdquo; you thought. &ldquo;Maybe I&rsquo;ll pick up an expansion or two; that will be plenty.&rdquo; But now, despite your efforts to the contrary, you own all the official content, and you&rsquo;e desperately need professionally printed copies of all the Arkham Horror community&rsquo;s homebrew content. You can no longer resist; it&rsquo;s time to accept your <strong>Inexorable Fate</strong>.</p>
+    </div> 
+  )
+}
