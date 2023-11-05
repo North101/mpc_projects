4b5d88f577c66cc130471a245ebfad570ff6237d
Alexander Wilde <alexander.wilde87@gmail.com>
2023-11-05 21:39:56 +0000

    Autogenerate help nav

--- src/client/components/HelpPage.tsx
+++ src/client/components/HelpPage.tsx
@@ -1,90 +1,69 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/esm/Row';
 import { AppContainer } from './AppContainer';
 import { HelpText } from './HelpText';
 
+function* nodeIterator(result: XPathResult) {
+  let thisNode = result.iterateNext();
+  while (thisNode) {
+    yield thisNode
+    thisNode = result.iterateNext();
+  }
+}
+
 const SideNav = () => {
-  return (
-    <Nav as='ul' className="flex-column">
-      <Nav.Item as='li'>Getting Started
-        <Nav as='ul' className="flex-column">
-          <Nav.Item as='li'>
-            <Nav.Link href="#install">Installing the Chrome Extension</Nav.Link>
+  const [nav, setNav] = useState<JSX.Element | null>(null)
+  useEffect(() => {
+    const items = []
+    const iterator = document.evaluate("//*[@class='help tldr']//section", document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
+    for (const node of nodeIterator(iterator)) {
+      const subitems = []
+      const iterator2 = document.evaluate(".//h3", node, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
+      for (const node2 of nodeIterator(iterator2)) {
+        subitems.push({
+          id: document.evaluate("./@id", node2, null, XPathResult.STRING_TYPE).stringValue,
+          name: document.evaluate("./text()", node2, null, XPathResult.STRING_TYPE).stringValue,
+        })
+      }
+      items.push({
+        name: document.evaluate(".//h2/text()", node, null, XPathResult.STRING_TYPE).stringValue,
+        items: subitems,
+      })
+    }
+    setNav((
+      <Nav as='ul' className="flex-column">
+        {items.map((e, index) => (
+          <Nav.Item key={index} as='li'>
+            {e.name}
+            {e.items.map((e, index) => (
+              <Nav key={index} as='ul' className="flex-column">
+                <Nav.Item as='li'>
+                  <Nav.Link href={`#${e.id}`}>{e.name}</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            ))}
           </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#next-steps">Next Steps</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Nav.Item>
-      <Nav.Item as='li'>Find Projects
-        <Nav as='ul' className="flex-column">
-          <Nav.Item as='li'>
-            <Nav.Link href="#download">Downloading a Project</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#search">Using Project Search</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#filter">Filtering Projects</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Nav.Item>
-      <Nav.Item as='li'>Load a Project
-        <Nav as='ul' className="flex-column">
-          <Nav.Item as='li'>
-            <Nav.Link href="#project-tab">Loading the Project from a .json file</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#project-tab">Combining and Editing Projects (Optional)</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#finalize">Finalizing and Uploading Your Project</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#view-on-mpc">Viewing Your Project on MPC</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Nav.Item>
-      <Nav.Item as='li'>Create a Project
-        <Nav as='ul' className="flex-column">
-          <Nav.Item as='li'>
-            <Nav.Link href="#image-prep">Preparing Images for Upload</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#image-upload">Uploading Images</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#creator-combine-edite">Combining and Editing Projects (Optional)</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href="#creator-view">Viewing Your Project on MPC</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Nav.Item>
-      <Nav.Item as='li'>FAQ
-        <Nav as='ul' className="flex-column">
-          <Nav.Item as='li'>
-            <Nav.Link href="#faq">Frequently Asked Questions</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Nav.Item>
-    </Nav>
-  )
+        ))}
+      </Nav>
+    ))
+  }, [])
+  return nav
 }
 
 export const HelpPage = () => {
   const hash = window.location.hash
   useEffect(() => {
+    if (!hash) return
     document.querySelector(hash)?.scrollIntoView()
   }, [hash])
 
   return (
     <AppContainer>
-      <Row className='gx-5' style={{maxWidth: '100%'}}>
+      <Row className='gx-5' style={{ maxWidth: '100%' }}>
         <aside className='col-md-3'>
-          <SideNav></SideNav>
+          <SideNav />
         </aside>
         <div className='tldr col-md-9'>
           <HelpText />
