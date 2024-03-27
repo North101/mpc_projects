bba2e908e71e4358fa61984904a640f020ffcb46
Alexander Wilde <alexander.wilde87@gmail.com>
2024-03-27 01:43:08 +0000

    Fix cookie styling poluting base styles

--- src/client/components/CookiePage.tsx
+++ src/client/components/CookiePage.tsx
@@ -6,7 +6,7 @@ type Result = 'success' | 'invalid_code' | 'invalid_cookie' | 'unknown' | null
 
 export const CookiePage = () => {
   const search = useSearch();
-  const [params, setParams] = useState<URLSearchParams|null>(null)
+  const [params, setParams] = useState<URLSearchParams | null>(null)
   const [cookie, setCookie] = useState('')
   const [result, setResult] = useState<Result>(null)
 
@@ -46,31 +46,33 @@ export const CookiePage = () => {
 
   return (
     <AppContainer showSearch={false}>
-      {result == 'success' && <div>Success</div>}
-      {result == 'invalid_code' && <div>Code is invalid</div>}
-      {result == 'invalid_cookie' && <div>Cookie is invalid</div>}
-      {result == 'unknown' && <div>Unknown error</div>}
+      <div className='cookie'>
+        {result == 'success' && <div>Success</div>}
+        {result == 'invalid_code' && <div>Code is invalid</div>}
+        {result == 'invalid_cookie' && <div>Cookie is invalid</div>}
+        {result == 'unknown' && <div>Unknown error</div>}
 
-      <form action='/set_cookie' onSubmit={onSubmit}>
-        <label htmlFor='cookie'>Cookie:</label><br />
-        <input type='text' name='cookie' id='cookie' value={cookie} onChange={onCookieChange} />
-        <br />
-        <input type='submit' value='Submit' onClick={onClick} />
-      </form>
-      <div className='instructions'>
-        <p><img src='/assets/images/mpc-cookie-manual.png' /></p>
-        <h3>To get the cookie:</h3>
-        <ol>
-          <li>Log into the Arkham Horror Homebrew MPC account.</li>
-          <li>Go to your <a href='https://www.makeplayingcards.com/design/dn_temporary_designes.aspx'>Saved Projects</a> page.</li>
-          <li>Open developer tools in your browser and go to the <strong>Application</strong> tab.</li>
-          <li>In the left panel, expand <strong>Cookies</strong> and click &ldquo;https://www/makeplayingcards.com&rdquo;</li>
-          <li>Search for <code>__p</code> and click on <code>__pcunck</code>.</li>
-          <li>Copy the <strong>Cookie Value</strong> displayed at the bottom of the dev tools tab.</li>
-          <li>Paste the cookie into the input above and submit.</li>
-          <li>Thank you for fighting the good fight to keep homebrew printing available!</li>
-        </ol>
-        <p className='note'>If you work at MPC please note that we are not violating anyone&rsquo;s copyright and not doing anything malicious. We only want to make it easier for people to use your service to print cards without the person who uploads the cards selling them at a profit, which violates the fair use of IP. This is a win-win!</p>
+        <form action='/set_cookie' onSubmit={onSubmit}>
+          <label htmlFor='cookie'>Cookie:</label><br />
+          <input type='text' name='cookie' id='cookie' value={cookie} onChange={onCookieChange} />
+          <br />
+          <input type='submit' value='Submit' onClick={onClick} />
+        </form>
+        <div className='instructions'>
+          <p><img src='/assets/images/mpc-cookie-manual.png' /></p>
+          <h3>To get the cookie:</h3>
+          <ol>
+            <li>Log into the Arkham Horror Homebrew MPC account.</li>
+            <li>Go to your <a href='https://www.makeplayingcards.com/design/dn_temporary_designes.aspx'>Saved Projects</a> page.</li>
+            <li>Open developer tools in your browser and go to the <strong>Application</strong> tab.</li>
+            <li>In the left panel, expand <strong>Cookies</strong> and click &ldquo;https://www/makeplayingcards.com&rdquo;</li>
+            <li>Search for <code>__p</code> and click on <code>__pcunck</code>.</li>
+            <li>Copy the <strong>Cookie Value</strong> displayed at the bottom of the dev tools tab.</li>
+            <li>Paste the cookie into the input above and submit.</li>
+            <li>Thank you for fighting the good fight to keep homebrew printing available!</li>
+          </ol>
+          <p className='note'>If you work at MPC please note that we are not violating anyone&rsquo;s copyright and not doing anything malicious. We only want to make it easier for people to use your service to print cards without the person who uploads the cards selling them at a profit, which violates the fair use of IP. This is a win-win!</p>
+        </div>
       </div>
     </AppContainer >
   )
