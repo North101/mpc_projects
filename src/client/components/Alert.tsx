a2d1b733cc89e8e323c068a835f0a5b9b65f1491
BrightBold <kelley@brightbold.com>
2024-10-03 18:40:35 -0400

    project IDs for Phantom

---
+++ src/client/components/Alert.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
+
+export const Alert = ({ showSearch, setSearch }: HeaderProps) => (
+  <div className='announcements'>Announcements</div>
+)
